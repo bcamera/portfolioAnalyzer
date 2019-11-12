9366512d4b24dccbba536bf882e20723f8ba5766
Bruno Camera <bcamera@hotmail.com>
2019-11-12 19:25:01 -0300

    replaced post with index.html

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-        path('', views.post_list, name='post_list'),
-        path('post/<int:pk>/', views.post_detail, name='post_detail'),
-        path('post/new/', views.post_new, name='post_new'),
-        path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-        path('post/1/<str:acao>/fronteira_eficiente', views.carrega_dados, name='carrega_dados'),
+        path('', views.post_detail, name='index'),
+        path('index.html', views.post_detail, name='index'),
+        path('post/<str:acao>/fronteira_eficiente', views.carrega_dados, name='carrega_dados'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -244,9 +244,9 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')     
     return render(request, 'blog/post_list.html', {'posts': posts})
     
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+def post_detail(request):
+    post = get_object_or_404(Post)
+    return render(request, 'blog/index.html', {'post': post})
     
 def post_new(request):
      if request.method == "POST":
